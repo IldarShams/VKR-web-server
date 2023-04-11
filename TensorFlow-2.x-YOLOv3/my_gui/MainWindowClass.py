fd1f5074eff79b1580027b5f9135acc297e9e2d0
Шамсутднинов Ильдар Рамилевич <i-shamsutdinov@inbox.ru>
2023-04-12 00:09:24 +0300

    Добавил обработку изображений в директории

--- TensorFlow-2.x-YOLOv3/my_gui/MainWindowClass.py
+++ TensorFlow-2.x-YOLOv3/my_gui/MainWindowClass.py
@@ -1,14 +1,13 @@
 import sys
 import cv2
+import os
 from pygrabber.dshow_graph import FilterGraph
 from multiprocessing import Queue, Lock
 from PyQt6 import QtCore, QtGui, QtWidgets, uic
 
 from PyQt6.QtCore import Qt
 
-
 from my_gui.Emitter import *
-
 
 qt_creator_file = "./my_gui/test.ui"
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qt_creator_file)
@@ -33,21 +32,66 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.nextButton.pressed.connect(self.send_image_to_yolo)
         self.emitter.image_available.connect(self.get_image_from_yolo)
         self.browserPathButton.pressed.connect(self.getImagesDirectory)
+        self.videoDevices.currentIndexChanged.connect(self.videoDeviceChanged)
+        self.browserPathLineEdit.textChanged.connect(self.imagesPathChanged)
+        self.images = None
+        self.browserPathLineEdit.setText("")
+
+        # test section
+        self.l = QtWidgets.QLabel()
+        self.test = QtWidgets.QComboBox()
+        self.but = QtWidgets.QPushButton()
+        self.but.setEnabled(True)
+
+        # print(self.imageLabel.size[0])
+        self.lineedit = QtWidgets.QLineEdit()
+        # test section
+
+    def videoDeviceChanged(self):
+        if self.videoDevices.currentIndex() == 0:
+            self.browserPathButton.setEnabled(True)
+            self.browserPathLineEdit.setEnabled(True)
+            self.nextButton.setEnabled(True)
+            self.backButton.setEnabled(True)
+
+        else:
+            self.browserPathButton.setEnabled(False)
+            self.browserPathLineEdit.setEnabled(False)
+            self.nextButton.setEnabled(False)
+            self.backButton.setEnabled(False)
 
     def getImagesDirectory(self):
-        self.browserPathLineEdit.setText(QtWidgets.QFileDialog.getExistingDirectory(self, "Выберете папку с изображениями",
-                                                   "./", QtWidgets.QFileDialog.Option.ShowDirsOnly))
+        self.browserPathLineEdit.setText(
+            QtWidgets.QFileDialog.getExistingDirectory(self, "Выберете папку с изображениями",
+                                                       "./", QtWidgets.QFileDialog.Option.ShowDirsOnly))
 
+    def imagesPathChanged(self):
+        if self.browserPathLineEdit.text() == "":
+            self.images = self.imageGenerator(None)
+        else:
+            self.images = self.imageGenerator(self.browserPathLineEdit.text())
 
     def send_image_to_yolo(self):
-        self.lock.acquire()
-        im = "C:\\Users\\User\\PycharmProjects\\CNN1\\TensorFlow-2.x-YOLOv3\\IMAGES\\B0015_0001.png"
-        self.to_yolo.put("image")
-        self.to_yolo.put(im)
-
+        try:
+            im = self.images.__next__()
+            self.lock.acquire()
+            # print(self.images.__next__())
+            self.to_yolo.put("image")
+            self.to_yolo.put(im)
+        except StopIteration as si:
+            print(str(si))
+            self.imagesPathChanged()
+            self.send_image_to_yolo()
+        except AttributeError as e:
+            print(str(e))
+            print("Путь не задан")
+        except Exception as e:
+            print(str(e))
+            print("Что то не так!")
+        # "C:\\Users\\User\\PycharmProjects\\CNN1\\TensorFlow-2.x-YOLOv3\\IMAGES\\B0015_0001.png"
 
     def get_image_from_yolo(self):
-        image = self.from_yolo.get()
+        image = self.resizeImage(self.from_yolo.get())
         qformat = QtGui.QImage.Format.Format_Indexed8
         if len(image.shape) == 3:
             if image.shape[2] == 4:
@@ -65,12 +109,43 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.lock.release()
 
     def closeEvent(self, event):
-        self.lock.acquire(block=True)
+        # self.lock.acquire(block=True)
         self.to_yolo.put("exit")
-        while True:
-            ex_ = self.from_yolo.get()
-            print(ex_ == "exit")
-            if ex_ == "exit":
-                break
-            continue
+        # while True:
+        #     ex_ = self.from_yolo.get()
+        #     print(ex_ == "exit")
+        #     if ex_ == "exit":
+        #         break
+        #     continue
         QtWidgets.QMainWindow.closeEvent(self, event)
+
+    def imageGenerator(self, path):
+        try:
+            for image in os.listdir(path):
+                if image.__contains__(".png") or image.__contains__(".jpg"):
+                    yield os.path.join(path, image)
+        except os.error:
+            print("os.error")
+
+    def resizeImage(self, image):
+        try:
+            w_w = self.imageLabel.size().width()
+            w_h = self.imageLabel.size().height()
+            print(w_w, w_h)
+            im_h = image.shape[0]
+            im_w = image.shape[1]
+            print(im_w, im_h)
+            if im_w > w_w:
+                k = im_w / w_w
+                im_w = w_w
+                im_h = round(im_h / k)
+            if im_h > w_h:
+                k = im_h / w_h
+                im_h = w_h
+                im_w = round(im_w / k)
+            print(im_w, im_h)
+            im = cv2.resize(image, (im_w, im_h))
+            return im
+        except Exception as e:
+            print(e)
+            return image
--- tests.py
+++ tests.py
@@ -297,37 +297,45 @@ from keras import datasets, layers, models, activations
 
 
 # pygrabber для поиска камер
+# import cv2
+# from pygrabber.dshow_graph import FilterGraph
+#
+# graph = FilterGraph()
+#
+# print(graph.get_input_devices())# list of camera device
+#
+# try:
+#     device =graph.get_input_devices().index("Lenovo EasyCamera")
+#
+# except ValueError as e:
+#
+#     device = graph.get_input_devices().index("Integrated Webcam")#use default camera if the name of the camera that I want to use is not in my list
+#
+# cap=cv2.VideoCapture(device)
+# if not cap.isOpened():
+#     print("Cannot open camera")
+#     exit()
+# while True:
+#     # Capture frame-by-frame
+#     ret, frame = cap.read()
+#     # if frame is read correctly ret is True
+#     if not ret:
+#         print("Can't receive frame (stream end?). Exiting ...")
+#         break
+#     # Our operations on the frame come here
+#     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#     # Display the resulting frame
+#     cv2.imshow('frame', gray)
+#     if cv2.waitKey(1) == ord('q'):
+#         break
+# # When everything done, release the capture
+# cap.release()
+# cv2.destroyAllWindows()
+
 import cv2
-from pygrabber.dshow_graph import FilterGraph
 
-graph = FilterGraph()
+im = cv2.imread("C:\\Users\\User\\PycharmProjects\\CNN1\\TensorFlow-2.x-YOLOv3\\IMAGES\\B0011_0001.png")
+print(im.shape)
+print(im.shape[0])
+print(im.shape[1])
 
-print(graph.get_input_devices())# list of camera device
-
-try:
-    device =graph.get_input_devices().index("Lenovo EasyCamera")
-
-except ValueError as e:
-
-    device = graph.get_input_devices().index("Integrated Webcam")#use default camera if the name of the camera that I want to use is not in my list
-
-cap=cv2.VideoCapture(device)
-if not cap.isOpened():
-    print("Cannot open camera")
-    exit()
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    # Display the resulting frame
-    cv2.imshow('frame', gray)
-    if cv2.waitKey(1) == ord('q'):
-        break
-# When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
